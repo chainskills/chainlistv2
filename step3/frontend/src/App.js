85f930a9224a9519b83657af9ed6ca0f7b1b6e9a
eloudsa <300818+eloudsa@users.noreply.github.com>
2021-12-20 16:50:21 +0100

    step 3: header, hero

--- step3/frontend/src/App.js
+++ step3/frontend/src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import { createTheme } from "@material-ui/core/styles";
+import Header from "./components/layout/Header";
+import Hero from "./components/layout/Hero";
 
-function App() {
-	return <div className="App"></div>;
-}
+const theme = createTheme({
+	palette: {
+		type: "light",
+	},
+});
+
+const App = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Header />
+			<Hero />
+		</ThemeProvider>
+	);
+};
 
 export default App;
