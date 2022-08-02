45d3ec6e033af98e832f98902283601ef30f7a9a
Said <300818+eloudsa@users.noreply.github.com>
2022-08-02 20:56:26 +0200

    from step17

---
+++ step18/backend/contracts/ChainList.sol
@@ -0,0 +1,209 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract ChainList {
+    // Custom types
+    struct Article {
+        uint256 id;
+        address payable owner;
+        string name;
+        string description;
+        uint256 price;
+    }
+
+    // State variables
+    address payable owner;
+    bool active;
+    uint256 x;
+    mapping(uint256 => Article) public articles;
+    uint256 articleCounter;
+
+    fallback() external {
+        x = 1;
+    }
+
+    // Events
+    event SellArticleEvent(
+        uint256 indexed _id,
+        address indexed _seller,
+        string _name,
+        uint256 _price
+    );
+
+    event BuyArticleEvent(
+        uint256 indexed _id,
+        address indexed _seller,
+        address indexed _buyer,
+        string _name,
+        uint256 _price
+    );
+
+    // Modifiers
+    modifier onlyActive() {
+        // ensure that the contract is active
+        require(active, "Not active");
+        _;
+    }
+
+    constructor() {
+        owner = payable(msg.sender);
+        active = true;
+    }
+
+    // check if address is the owner
+    function isOwner() public view returns (bool) {
+        return (msg.sender == owner);
+    }
+
+    // Activate or deactivate the contract
+    // Only allowed to the contract's owner
+    function activate(bool _active) public {
+        require(msg.sender == owner, "Only allowed to the contract's owner");
+        active = _active;
+    }
+
+    // check if contract is active
+    function isActive() public view returns (bool) {
+        return (active);
+    }
+
+    // sell a new article
+    function sellArticle(
+        string memory _name,
+        string memory _description,
+        uint256 _price
+    ) public onlyActive {
+        // a name is required
+        bytes memory name = bytes(_name);
+        require(name.length > 0, "A name is required");
+
+        // a description is required
+        bytes memory description = bytes(_description);
+        require(description.length > 0, "A description is required");
+
+        // the price can be 0 if seller decides to give the article
+
+        // a new article
+        articleCounter++;
+
+        // store this article
+        articles[articleCounter] = Article(
+            articleCounter,
+            payable(msg.sender),
+            _name,
+            _description,
+            _price
+        );
+
+        // trigger the event to inform that a new article is for sale
+        emit SellArticleEvent(articleCounter, msg.sender, _name, _price);
+    }
+
+    // buy an article
+    function buyArticle(uint256 _id) public payable onlyActive {
+        // we check whether there is an article for sale
+        require(articleCounter > 0, "No articles to buy");
+
+        // we check that the article exists
+        require(_id > 0 && _id <= articleCounter, "Article doesn't exist");
+
+        // we retrieve the article
+        Article storage article = articles[_id];
+
+        // we don't allow the seller to buy his own article
+        require(
+            msg.sender != article.owner,
+            "Seller cannot buy his own article"
+        );
+
+        // we check that the value sent corresponds to the price of the article
+        require(msg.value == article.price, "Price doesn't match");
+
+        // keep the seller address
+        address payable seller = article.owner;
+
+        // the buyer can pay the seller
+        seller.transfer(msg.value);
+
+        // the buyer becomes the new owner
+        article.owner = payable(msg.sender);
+
+        // trigger the event that the article is sold
+        emit BuyArticleEvent(
+            _id,
+            seller,
+            msg.sender,
+            article.name,
+            article.price
+        );
+
+        // trigger the event to inform that a new article is for sale
+        emit SellArticleEvent(_id, msg.sender, article.name, article.price);
+    }
+
+    // fetch the number of articles in the contract
+    function getNumberOfArticles() public view returns (uint256) {
+        return articleCounter;
+    }
+
+    // fetch and return all articles for sale not owned by the current account
+    function getMarketplace()
+        public
+        view
+        onlyActive
+        returns (Article[] memory)
+    {
+        // prepare output array
+        uint256[] memory articleIds = new uint256[](articleCounter);
+
+        uint256 nbArticles = 0;
+        // iterate over articles
+        for (uint256 i = 1; i <= articleCounter; i++) {
+            // keep the ID if the article is for sale
+            if (
+                (articles[i].owner != msg.sender) &&
+                (articles[i].owner != address(0x0))
+            ) {
+                articleIds[nbArticles] = articles[i].id;
+                nbArticles++;
+            }
+        }
+
+        // returns all the articles for sale
+        return (buildArticles(articleIds, nbArticles));
+    }
+
+    // fetch and return all articles for sale owned by the current account
+    function getMyArticles() public view onlyActive returns (Article[] memory) {
+        // prepare output array
+        uint256[] memory articleIds = new uint256[](articleCounter);
+
+        uint256 nbArticles = 0;
+        // iterate over articles
+        for (uint256 i = 1; i <= articleCounter; i++) {
+            // keep the ID if the article is still for sale
+            if (articles[i].owner == msg.sender) {
+                articleIds[nbArticles] = articles[i].id;
+                nbArticles++;
+            }
+        }
+
+        // returns all your articles
+        return (buildArticles(articleIds, nbArticles));
+    }
+
+    // build an array of articles
+    function buildArticles(uint256[] memory articleIds, uint256 nbArticles)
+        internal
+        view
+        returns (Article[] memory)
+    {
+        Article[] memory allArticles = new Article[](nbArticles);
+        for (uint256 i = 0; i < nbArticles; i++) {
+            Article memory _article = articles[articleIds[i]];
+            // we don't cannot use push() because this function is not allowed memory types
+            allArticles[i] = _article;
+        }
+        return allArticles;
+    }
+}
---
+++ step18/frontend/src/components/articles/Articles.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import {
+	Container,
+	Grid,
+	Fab,
+	Box,
+	Typography,
+	Tabs,
+	Tab,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import ArticleDialog from "./ArticleDialog";
+import ArticleCard from "./ArticleCard";
+import { useStore } from "context/StoreProvider";
+import {
+	getMyArticles,
+	getMarketplace,
+	sellArticle,
+	buyArticle,
+} from "context/web3Actions";
+
+const Articles = () => {
+	const [state, dispatch] = useStore();
+
+	// flag used to display or hide the modal dialog box
+	const [isOpen, setIsOpen] = useState(false);
+
+	const [selectedTab, setSelectedTab] = useState(1);
+
+	const handleChangeSelector = async (event, newSelection) => {
+		setSelectedTab(newSelection);
+	};
+
+	// manage the display of the dialog box
+	const handleOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
+	useEffect(() => {
+		async function fetchArticles() {
+			if (state.account !== null) {
+				if (selectedTab === 0) {
+					/* It's a debugging statement. */
+					// marketplace
+					await getMarketplace(state, dispatch);
+				} else {
+					await getMyArticles(state, dispatch);
+				}
+			}
+		}
+		fetchArticles();
+		// eslint-disable-next-line
+	}, [state.account, selectedTab]);
+
+	// called when we add the article to be sold
+	const onSellArticle = async (_article) => {
+		await sellArticle(state, dispatch, _article);
+	};
+
+	// called when we buy the article
+	const onBuyArticle = async (article) => {
+		await buyArticle(state, dispatch, article);
+	};
+
+	return (
+		<div>
+			{state.active && state.account !== null && state.allowed && (
+				<div>
+					<Container>
+						<Tabs
+							value={selectedTab}
+							onChange={handleChangeSelector}
+							indicatorColor="primary"
+							textColor="primary"
+							centered
+							sx={{
+								marginBottom: "20px",
+							}}
+						>
+							<Tab label="Marketplace" />
+							<Tab label="My Articles" />
+						</Tabs>
+					</Container>
+
+					<Container fixed>
+						<Fab
+							aria-label="add"
+							color="primary"
+							onClick={handleOpen}
+							sx={{
+								position: "fixed",
+								bottom: 16,
+								right: 16,
+							}}
+						>
+							<AddIcon />
+						</Fab>
+
+						<ArticleDialog
+							isDialogOpened={isOpen}
+							handleCloseDialog={() => setIsOpen(false)}
+							handleSaveDialog={onSellArticle}
+						/>
+
+						{(state.account === null || state.articles.length === 0) && (
+							<Box component="span" m={1}>
+								<Typography align="center" color="inherit" variant="h6">
+									No articles to display
+								</Typography>
+							</Box>
+						)}
+
+						{state.account !== null && (
+							<Grid container spacing={4}>
+								{state.articles.map((article) => (
+									<ArticleCard
+										key={article.id}
+										article={article}
+										account={state.account}
+										handleBuyArticle={onBuyArticle}
+									/>
+								))}
+							</Grid>
+						)}
+					</Container>
+				</div>
+			)}
+
+			{!state.active && state.account !== null && state.allowed && (
+				<Typography align="center" color="error" variant="h3">
+					Unable to fetch the contract
+				</Typography>
+			)}
+		</div>
+	);
+};
+
+export default Articles;
