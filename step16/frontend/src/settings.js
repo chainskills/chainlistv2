450c8ced85aef967d068a83199bdc6ce0e2a8f40
eloudsa <300818+eloudsa@users.noreply.github.com>
2022-08-21 17:12:42 +0100

    refactor

---
+++ step16/frontend/src/settings.js
@@ -0,0 +1,6 @@
+const settings = {
+	contractAddressPrivate: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+	contractAddressGoerli: "0x412d065bE197AeC2191291FB71846aD228E73B35",
+};
+
+export default settings;
