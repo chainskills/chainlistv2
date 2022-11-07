06252f797cf49278d9d018d8542c94f35aa7c43a
Said <300818+eloudsa@users.noreply.github.com>
2022-11-07 19:15:01 +0100

    update dependencies, deploy to firebase

--- step20/frontend/src/settings.js
+++ step20/frontend/src/settings.js
@@ -1,6 +1,7 @@
 const settings = {
 	contractAddressPrivate: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-	contractAddressGoerli: "0x412d065bE197AeC2191291FB71846aD228E73B35",
+	contractAddressGoerliOLD: "0x412d065bE197AeC2191291FB71846aD228E73B35",
+	contractAddressGoerli: "0x7916411CfA69019a79105B1E1C9AdBF9ae094cc7",
 };
 
 export default settings;
