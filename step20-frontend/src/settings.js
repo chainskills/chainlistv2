95d74dabf623a12ca47dbbfcdd825c0ca440581f
Said <300818+eloudsa@users.noreply.github.com>
2022-11-07 19:39:39 +0100

    remove unused folders

--- step20-frontend/src/settings.js
+++
@@ -1,7 +0,0 @@
-const settings = {
-	contractAddressPrivate: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-	contractAddressGoerliOLD: "0x412d065bE197AeC2191291FB71846aD228E73B35",
-	contractAddressGoerli: "0x7916411CfA69019a79105B1E1C9AdBF9ae094cc7",
-};
-
-export default settings;
