f834b9bc62f68a82e053f0bf2df162aa6234dffd
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2023-12-23 09:19:20 +0700

    Update ChainListHappyPath.js
    Fixed for ethers v6

--- step2/backend/test/ChainListHappyPath.js
+++ step2/backend/test/ChainListHappyPath.js
@@ -12,23 +12,21 @@ describe("ChainList - Happy Path", function () {
   before("setup contract for each test", async () => {
     const ChainList = await ethers.getContractFactory("ChainList");
     chainListInstance = await ChainList.deploy();
-    await chainListInstance.deployed();
+    await chainListInstance.waitForDeployment();
     [owner, seller] = await ethers.getSigners();
   });
 
   it("should be initialized with empty values", async () => {
     const article = await chainListInstance.getArticle();
 
-    expect(article._owner, "owner must be empty").to.equal(
-      ethers.constants.AddressZero
-    );
+    expect(article._owner, "owner must be empty").to.equal(ethers.ZeroAddress);
     expect(article._name, "article's name must be empty").to.equal("");
     expect(
       article._description,
-      "article's description must be empty"
+      "article's description must be empty",
     ).to.equal("");
     expect(article._price, "article's price must be zero").to.equal(
-      ethers.constants.Zero
+      BigInt("0"),
     );
   });
 
@@ -38,7 +36,7 @@ describe("ChainList - Happy Path", function () {
       .sellArticle(
         articleName1,
         articleDescription1,
-        ethers.utils.parseEther(articlePrice1.toString())
+        ethers.parseEther(articlePrice1.toString()),
       );
 
     // wait for the transaction to complete
@@ -47,15 +45,15 @@ describe("ChainList - Happy Path", function () {
     // check the article
     const article = await chainListInstance.getArticle();
     expect(article._name, "article name must be " + articleName1).to.equal(
-      articleName1
+      articleName1,
     );
     expect(
       article._description,
-      "article description must be " + articleDescription1
+      "article description must be " + articleDescription1,
     ).to.equal(articleDescription1);
     expect(
       article._price,
-      "article price must be " + articlePrice1 + " ETH"
-    ).to.equal(ethers.utils.parseEther(articlePrice1.toString()));
+      "article price must be " + articlePrice1 + " ETH",
+    ).to.equal(ethers.parseEther(articlePrice1.toString()));
   });
 });
